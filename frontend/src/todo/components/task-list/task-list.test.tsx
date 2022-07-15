3956314ab74b10830288df558e84523cc5351639
changmin <cckdals222@gmail.com>
2022-07-15 18:07:36 +0900

    refactor: test code 리팩토링

--- frontend/src/todo/components/task-list/task-list.test.tsx
+++ frontend/src/todo/components/task-list/task-list.test.tsx
@@ -6,19 +6,18 @@ import "@testing-library/jest-dom"
 import { TaskList } from "./task-list"
 
 describe("TaskList Component", () => {
-  it("rendering test", () => {
-    const data = [
-      { id: 1, title: "hellllo", completed: false, deleteText: () => console.log("helllo") },
-      { id: 2, title: "점심 식사 구하기", completed: true, deleteText: () => console.log("hihihi") },
-      { id: 3, title: "공부 계획 세우기", completed: false, deleteText: () => console.log("third text") },
-    ]
-    render(<TaskList userData={data} />)
-    const firstText = screen.getByText("hellllo")
-    const secondText = screen.getByText("점심 식사 구하기")
-    const thirdText = screen.getByText("공부 계획 세우기")
+  describe("컴포넌트 랜더링됐을때 화면", () => {
+    it("text들이 화면에 랜더링 나오는지 확인", () => {
+      const data = [
+        { id: 1, title: "hellllo", completed: false, deleteText: () => console.log("helllo") },
+        { id: 2, title: "점심 식사 구하기", completed: true, deleteText: () => console.log("hihihi") },
+        { id: 3, title: "공부 계획 세우기", completed: false, deleteText: () => console.log("third text") },
+      ]
+      render(<TaskList userData={data} />)
 
-    expect(firstText).toBeInTheDocument()
-    expect(secondText).toBeInTheDocument()
-    expect(thirdText).toBeInTheDocument()
+      const propsText = screen.getByText("점심 식사 구하기")
+
+      expect(propsText).toBeInTheDocument()
+    })
   })
 })
