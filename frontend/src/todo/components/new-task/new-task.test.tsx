f64201669b56dbccf3cf0dcadda26179ec4bf1a4
changmin <cckdals222@gmail.com>
2022-07-18 09:29:32 +0900

    refactor: new-task 테스트 코드 수정

--- frontend/src/todo/components/new-task/new-task.test.tsx
+++ frontend/src/todo/components/new-task/new-task.test.tsx
@@ -1,36 +1,47 @@
 import React from "react"
-import { render, screen, fireEvent } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import "@testing-library/jest-dom"
 
 import { NewTask } from "./new-task"
-import Home from "../../../pages/index"
 
 describe("newTask component test", () => {
-  it("test add input text to props data", () => {
-    const mockSetState = jest.fn()
-    const data = [
-      { id: 1, title: "hello", completed: false, deleteText: () => console.log("deleteText") },
-      { id: 2, title: "저녁 메뉴 정하기", completed: true, deleteText: () => console.log("this is delete function") },
-      { id: 3, title: "운동 계획 세우기", completed: false, deleteText: () => console.log(3) },
-    ]
+  describe("유저가 새로운 task를 작성", () => {
+    it("엔터키를 눌렀을때 setData함수가 실행되는지 확인, input값 초기화", () => {
+      const mockSetState = jest.fn()
+      const mockAllCheckFn = jest.fn()
+      const data = [
+        { id: 1, title: "hello", completed: false, deleteText: () => console.log("deleteText") },
+        { id: 2, title: "저녁 메뉴 정하기", completed: true, deleteText: () => console.log("this is delete function") },
+        { id: 3, title: "운동 계획 세우기", completed: false, deleteText: () => console.log(3) },
+      ]
 
-    render(<NewTask setData={mockSetState} userData={data} />)
+      render(<NewTask setData={mockSetState} userData={data} setAllChecked={mockAllCheckFn} />)
 
-    const textInput = screen.getByRole("textbox")
-    userEvent.type(textInput, "새로운 데이터를 추가")
-    userEvent.type(textInput, "{enter}")
+      const textInput = screen.getByRole("textbox")
+      userEvent.type(textInput, "새로운 데이터를 추가")
+      userEvent.type(textInput, "{enter}")
 
-    const textInfo = {
-      id: 4,
-      title: "새로운 데이터를 추가",
-      completed: false,
-      deleteText: () => console.log("deleteButton"),
-    }
-    const newUserData = data.concat(textInfo)
+      expect(textInput).toHaveValue("")
+      expect(mockSetState).toHaveBeenCalled()
+    })
+  })
 
-    expect(textInput).toHaveValue("")
-    expect(mockSetState).toHaveBeenCalled()
-    // expect(mockSetState(data, textInfo)).toEqual(newUserData)
+  describe("유저가 all check 버튼 클릭", () => {
+    it("all check 실행 확인", () => {
+      const mockAllCheckFn = jest.fn()
+      const mockSetState = jest.fn()
+      const data = [
+        { id: 1, title: "hello", completed: false, deleteText: () => console.log("deleteText") },
+        { id: 2, title: "저녁 메뉴 정하기", completed: true, deleteText: () => console.log("this is delete function") },
+        { id: 3, title: "운동 계획 세우기", completed: false, deleteText: () => console.log(3) },
+      ]
+      render(<NewTask setData={mockSetState} userData={data} setAllChecked={mockAllCheckFn} />)
+
+      const checkButton = screen.getByRole("button")
+      userEvent.click(checkButton)
+
+      expect(mockAllCheckFn).toHaveBeenCalled()
+    })
   })
 })
